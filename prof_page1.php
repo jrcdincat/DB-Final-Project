8e13ef5128dc5fa1400ba356e07b772f46ed8bf6
DESKTOP-ADFBECM\jrcdi <jrcdincat@gmail.com>
2021-12-04 16:45:02 -0500

    Add edit button popup if form doesn't exist

--- prof_page1.php
+++ prof_page1.php
@@ -10,8 +10,11 @@
         echo "cookie not set";
     }
 
-    // Set visibility of add new books form
+    // Set visibility overwrite form popup
     $is_overwrite_popup_visible= false;
+
+    // Set visibility of create new form popup
+    $is_new_popup_visible = false;
 
     if(isset($_POST['no_overwrite']))
     {
@@ -22,6 +25,17 @@
         // Go to next page after deleting previous form data
         header('Location: prof_page2.php');
     }        
+
+    // Edit form popup button responses
+    if(isset($_POST['yes_new']))
+    {
+        header('Location: prof_page2.php');
+    }     
+    if(isset($_POST['no_new']))
+    {
+        $is_new_popup_visible = false;
+    }       
+
 
     $email = $_COOKIE['email'];
     $formID = $_COOKIE['formID'];
@@ -37,6 +51,11 @@
             // Request confirmation to overwrite form
             $is_overwrite_popup_visible= true;    
         }
+        else
+        {
+            // No form exists, proceed to next page
+            header('Location: prof_page2.php');
+        }
 
         mysqli_free_result($result);
     }
@@ -45,20 +64,17 @@
     {
         $sql = "SELECT * FROM forms where formID = '".$formID."'";
         $result = mysqli_query($conn, $sql);
+
         // Check if form already exists, if not, prompt to create new form
         if (mysqli_num_rows($result) > 0)
         {
-            // Request deletion first
-            echo "record found";
+            header('Location: prof_page2.php');
         }
-        // else
-        // {
-        //     // Go to next page to add books without populating values
-        //     echo "No records";
-        // }
-
-        // Send to form edit
-        //header('Location: prof_page2.php');
+        else
+        {
+            // Request confirmation to create new form
+            $is_new_popup_visible = true;    
+        }
     }
 
     mysqli_close($conn);
@@ -68,24 +84,34 @@
     <!-- Add header to webpage -->
     <?php include ('templates/header.php'); ?>
         <section class="container" style="text-align: center;">
-                <h1>
-                    <?php echo "Select Option"; ?>
-                </h1>
-                <form action=prof_page1.php method="POST">
-                    <div class="center" style="margin:15px; text-align:center;">
-                            <input type="submit" name="new_form_submit" value="New Form">
-                    </div>
-                    <div class="center" style="margin:15px; text-align:center;">
-                            <input type="submit" name="edit_form_submit" value="Edit Form">
-                    </div>
-                </form>
-                <!-- Popup -->
-                <div class="form-popup" id="delete_form" <?php if($is_overwrite_popup_visible){echo 'style="display:inline;"';}?>>
+            <h1>
+                <?php echo "Select Option"; ?>
+            </h1>
+            <form action=prof_page1.php method="POST">
+                <div class="center" style="margin:15px; text-align:center;">
+                        <input type="submit" name="new_form_submit" value="New Form">
+                </div>
+                <div class="center" style="margin:15px; text-align:center;">
+                        <input type="submit" name="edit_form_submit" value="Edit Form">
+                </div>
+            </form>
+            <!-- Overwrite Popup -->
+            <div class="form-popup" id="delete_form" <?php if($is_overwrite_popup_visible){echo 'style="display:inline;"';}?>>
                 <form action='prof_page1.php' method="POST" class="form-container">
                     <h1>A form already exists and will be overwritten. Would you like to continue?</h1>
                     <button name="yes_overwrite" type="submit" class="btn">Continue</button>
                     <form action="prof_page1.php" method="POST">
                         <button name="no_overwrite" type="submit" class="btn cancel">Cancel</button>
+                    </form>
+                </form>
+            </div>
+            <!-- New Popup -->
+            <div class="form-popup" id="new_form" <?php if($is_new_popup_visible){echo 'style="display:inline;"';}?>>
+                <form action='prof_page1.php' method="POST" class="form-container">
+                    <h1>Form Not Found. Would you like to create a new form?</h1>
+                    <button name="yes_new" type="submit" class="btn">Continue</button>
+                    <form action="prof_page1.php" method="POST">
+                        <button name="no_new" type="submit" class="btn cancel">Cancel</button>
                     </form>
                 </form>
             </div>
