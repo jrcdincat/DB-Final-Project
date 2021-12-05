d5ddc58b0df70f2026dc15b8da9f74d74744b05f
Nate8888 <wilk.nathan@gmail.com>
2021-12-05 00:45:02 -0500

    Created Admin Table to view admins, delete, and add new ones

--- staff_page2.php
+++ staff_page2.php
@@ -32,20 +32,19 @@
     <?php include ('templates/header.php'); ?>
         <section class="container" style="text-align: center;">
         <table style="width:100%">
-                    <h1>Add a new ADMIN to the Database</h1>
+                    <h1>Add a Faculty</h1>
                     <!-- Create a form with three inputs that take the first name, last name, and email of the faculty -->
                     <form action="staff_page1.php" method="post">
                         <input type="text" name="f_name" placeholder="First Name" required>
                         <input type="text" name="l_name" placeholder="Last Name" required>
                         <input type="email" name="email" placeholder="Email" required>
-                        <input type="password" name="t_pass" placeholder="Temporary Password" required>
-                        <input type="submit" value="Add ADMIN">
+                        <input type="submit" value="Add Faculty">
                     </form>
                     <hr>
-                    <h1>Administrator List</h1>
+                    <h1>Faculty List</h1>
                     <thead>
                         <tr>
-                            <th style="width: 3%;">ALL <input id='maincheck' type='checkbox' name='adm[]'  checked></th>
+                            <th style="width: 3%;">ALL <input id='maincheck' type='checkbox' name='professor_id[]'  checked></th>
                             <th style="width:18%">First Name</th>
                             <th style="width:18%">Last Name</th>
                             <th style="width:18%">E-mail</th>
@@ -53,14 +52,14 @@
                     </thead>
                     <tbody>
                         <?php
-                                $sql = "SELECT * FROM admin";
+                                $sql = "SELECT * FROM Professors";
                                 $results = mysqli_query($conn, $sql);
-                                $admin_data = mysqli_fetch_all($results, MYSQLI_ASSOC);
+                                $professor_data = mysqli_fetch_all($results, MYSQLI_ASSOC);
 
-                                // Loop through each admin display f_name, l_name, and email
-                                foreach ($admin_data as $data) {
+                                // Loop through each professor display f_name, l_name, and email
+                                foreach ($professor_data as $data) {
                                     echo "<tr>";
-                                    echo "<td><input class='adm' type='checkbox' name='adm[]' value='" . $data['Email'] . "' checked></td>";
+                                    echo "<td><input class='prof' type='checkbox' name='professor_id[]' value='" . $data['Email'] . "' checked></td>";
                                     echo "<td>".$data['f_name']."</td>";
                                     echo "<td>".$data['l_name']."</td>";
                                     echo "<td>".$data['Email']."</td>";
@@ -72,7 +71,9 @@
                         mysqli_close($conn);
                     ?>
                 </table>
-                <button id="deleteselected" type="submit" name="delete" class="btn btn-primary">DELETE SELECTED ADMIN</button>
+                <!-- Create a button to submit the form containing all of the professors that are checked -->
+                <button id="submit" type="submit" name="sendemail" class="btn btn-primary">SEND EMAIL(S)</button>
+                <button id="deleteselected" type="submit" name="delete" class="btn btn-primary">DELETE SELECTED</button>
         </section>
     <?php include ('templates/footer.php'); ?>
 
@@ -86,12 +87,25 @@
             }
         });
 
-        // When the deleteselected button is clicked, check to see if any checkboxes are checked and if so, save the email of the checked checkboxes to a cookie
-        $('#deleteselected').click(function(){
-            var checked = $('input[class="adm"]:checked').length;
+        // When the submit button is clicked, check to see if any checkboxes are checked and if so, save the email of the checked checkboxes to a cookie
+        $('#submit').click(function(){
+            var checked = $('input[type="checkbox"]:checked').length;
             if(checked > 0){
                 var emails = [];
-                $('input[class="adm"]:checked').each(function(){
+                $('input[class="prof"]:checked').each(function(){
+                    emails.push($(this).val());
+                });
+                // TODO: PROCESS THE EMAILS TO PASS TO NEXT PAGE
+                console.log(emails);
+            }
+        });
+
+        // When the deleteselected button is clicked, check to see if any checkboxes are checked and if so, save the email of the checked checkboxes to a cookie
+        $('#deleteselected').click(function(){
+            var checked = $('input[class="prof"]:checked').length;
+            if(checked > 0){
+                var emails = [];
+                $('input[class="prof"]:checked').each(function(){
                     emails.push($(this).val());
                 });
 
