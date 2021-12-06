488a39b052cf8c969b6013d8820dabe69bca2dc6
Nate8888 <wilk.nathan@gmail.com>
2021-12-05 20:12:32 -0500

    Generate a temporary password and send an email with it. with Min

--- PHPMailer/staffRecovery.php
+++ PHPMailer/staffRecovery.php
@@ -8,50 +8,60 @@ require_once "PHPMailer/SMTP.php";
 require_once "PHPMailer/Exception.php";
 
     $email = $_POST['email'];
-    $rand = rand(100000,999999);
-    // UPDATE the password and temporary password to match the random number on the admin table
-    $sql = "UPDATE admin SET pword = '$rand', temp_pass = '$rand' WHERE email = '$email'";
+    // Check if the email exists on the admin database
+    $sql = "SELECT * FROM admin WHERE email = '$email'";
     $result = mysqli_query($conn, $sql);
-    if($result){
-        $body = "Here's your temporary password: '$rand'. Please login and change your password.";
-        $mail = new PHPMailer(true);
-
-        //smtp server settings
-        $mail->isSMTP();                                //Send Using SMTP
-        $mail->Host = "smtp.gmail.com";                 //SMTP server
-        $mail->SMTPAuth = true;                         //Enables SMTP authentication
-        $mail->Username = "mkimproject4710@gmail.com";  //Email address thats sending email
-        $mail->Password = 'Gr456987!';                  //Stop looking at my password! lol its a temp account made recently
-        $mail->Port = 465;                              //TCP port to connect to
-        $mail->SMTPSecure = "ssl";                      //SSL encryption
-
-        //email settings
-        $mail->isHTML(true);                            //Sets email format to HTML
-        $mail->setFrom("mkimproject4710@gmail.com", "Book Staff");    //Format of email
-        $mail->Subject = ("Password Recovery");                  //Subject of email
-        $mail->addAddress("$email");    //Sends to this email address
-        $mail->Body = $body;                            //Text area of email
-
-
-        //Send the email
-        if($mail->send()){
-            $status = "success";
-            $response = "Email is sent!";
-        }
-        else
-        {   
-            $status = "failed";
-            $response = "Something is wrong: <br>" . $mail->ErrorInfo;
-        }
-
+    $resultCheck = mysqli_num_rows($result);
+    if($resultCheck < 1){
+        $status = "failed";
+        $response = "email is not an admin:" . $conn->error;
         exit(json_encode(array("status" => $status, "response" => $response)));
     }
     else{
-        $status = "failed";
-        $response = "Something is wrong: <br>" . $conn->error;
-        exit(json_encode(array("status" => $status, "response" => $response)));
+        $rand = rand(100000,999999);
+        // UPDATE the password and temporary password to match the random number on the admin table
+        $sql = "UPDATE admin SET pword = '$rand', temp_pass = '$rand' WHERE email = '$email'";
+        $result = mysqli_query($conn, $sql);
+        if($result){
+            $body = "Here's your temporary password: '$rand'. Please login and change your password.";
+            $mail = new PHPMailer(true);
+
+            //smtp server settings
+            $mail->isSMTP();                                //Send Using SMTP
+            $mail->Host = "smtp.gmail.com";                 //SMTP server
+            $mail->SMTPAuth = true;                         //Enables SMTP authentication
+            $mail->Username = "mkimproject4710@gmail.com";  //Email address thats sending email
+            $mail->Password = 'Gr456987!';                  //Stop looking at my password! lol its a temp account made recently
+            $mail->Port = 465;                              //TCP port to connect to
+            $mail->SMTPSecure = "ssl";                      //SSL encryption
+
+            //email settings
+            $mail->isHTML(true);                            //Sets email format to HTML
+            $mail->setFrom("mkimproject4710@gmail.com", "Book Staff");    //Format of email
+            $mail->Subject = ("Password Recovery");                  //Subject of email
+            $mail->addAddress("$email");    //Sends to this email address
+            $mail->Body = $body;                            //Text area of email
+
+
+            //Send the email
+            if($mail->send()){
+                $status = "success";
+                $response = "Email is sent!";
+            }
+            else
+            {   
+                $status = "failed";
+                $response = "Something is wrong:" . $mail->ErrorInfo;
+            }
+
+            exit(json_encode(array("status" => $status, "response" => $response)));
+        }
+        else{
+            $status = "failed";
+            $response = "Something is wrong:" . $conn->error;
+            exit(json_encode(array("status" => $status, "response" => $response)));
+        }
     }
-    
 
 ?>
       
--- staff_password.php
+++ staff_password.php
@@ -2,17 +2,49 @@
 <html>
     <!-- Add header to webpage -->
     <?php include ('templates/header.php'); ?>
+        <h4 class="sent-notification"></h4>
         <section class="container" style="text-align: center;">
             <h1>
                 <?php echo "Forgot Password"; ?>
             </h1>
-            <form class="white" action="PHPMailer/staffRecovery.php" method="post" style="text-align: center;" >
                 <label>Email:</label>
-                <input type="text" name="email">
-                <div class="center" style="margin:15px; text-align:center; padding-left:140px">
-                    <input type="submit" name="submit" value=Submit>
-                </div>
-            </form>
+                <input type="text" id="email" name="email">
+                <button type="button" onclick="sendEmail()" value="Send An Email">Request Temporary Password</button>
+
         </section>
     <?php include ('templates/footer.php'); ?>
+
+    <script src="http://code.jquery.com/jquery-3.3.1.min.js"></script>
+    <script type="text/javascript">
+        function sendEmail() {
+            var email = $("#email").val();
+            if (email != "") {
+                $.ajax({
+                   url: 'PHPMailer/staffRecovery.php',
+                   method: 'POST',
+                   dataType: 'json',
+                   data: {
+                        email: email
+                   }, success: function (response) {
+                       //check if the response status is equal to fail
+                          if (response.status == "failed") {
+                            //if it is, display the error message
+                            $(".sent-notification").text(response.response);
+                          } else {
+                            //if it is not, display the success message
+                            $(".sent-notification").text(response.response + "\nPlease check your email for your temporary password.\nRedirection you to login page in 5 seconds.");
+                            // wait 5 seconds and redirect to login page
+                            setTimeout(function(){
+                                window.location.href = "staff.php";
+                            }, 5000);
+
+                          }
+                        // Reset value of email input   field
+                        $("#email").val("");
+                   }
+                });
+            }
+        }
+    </script>
+
 </html>
